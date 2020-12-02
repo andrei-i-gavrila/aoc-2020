b45941f9d05f93dd7a99616cf19f34befdb2a804
Andrei Gavrila <Andrei.Gavrila@cloudflight.io>
2020-12-02 07:12:45 +0200

    day 2 00:04:01   391 00:05:40   219

--- src/main/kotlin/ro/codespace/aoc/day2/Day2a.kt
+++ src/main/kotlin/ro/codespace/aoc/day2/Day2a.kt
@@ -1,5 +1,17 @@
 package ro.codespace.aoc.day2
 
+import ro.codespace.aoc.day
+
 fun main() {
-    TODO()
+    val passwords = day(2).readLines()
+    passwords.count {
+        val (lim, let, password)  = it.split(" ");
+
+
+        val (low, hi) = lim.split("-").map { it.toInt() }
+        val letter = let.substring(0, 1)
+
+
+        password.count { it.toString() == letter } in low..hi
+    }.let { println(it) }
 }
--- src/main/kotlin/ro/codespace/aoc/day2/Day2b.kt
+++ src/main/kotlin/ro/codespace/aoc/day2/Day2b.kt
@@ -1,5 +1,17 @@
 package ro.codespace.aoc.day2
 
+import ro.codespace.aoc.day
+
 fun main() {
-    TODO()
+    val passwords = day(2).readLines()
+    passwords.count {
+        val (lim, let, password)  = it.split(" ");
+        val letter = let.substring(0, 1)
+
+
+        val positions = lim.split("-").map { it.toInt() }
+
+        positions.count { password[it-1].toString() == letter } == 1
+
+    }.let { println(it) }
 }
