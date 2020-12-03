559354897442c8ba187dd5bfb718e77cb660424f
Andrei Gavrila <Andrei.Gavrila@cloudflight.io>
2020-12-03 07:37:51 +0200

    day 3 - Why kotlin, why? 00:17:24  4257 00:22:36  3554

--- src/main/kotlin/ro/codespace/aoc/day3/Day3a.kt
+++ src/main/kotlin/ro/codespace/aoc/day3/Day3a.kt
@@ -1,5 +1,24 @@
 package ro.codespace.aoc.day3
 
+import ro.codespace.aoc.day
+
+//kotlin sucks "123".split("") == ["1", "2", "3", ""] :\
+
 fun main() {
-    TODO()
+    val pattern = day(3).readLines().map { it.toList() }
+    var posX = 0
+    var posY = 0
+    val incX = 3
+    val incY = 1
+    var treeshit = 0
+    while (posY <= pattern.size) {
+        posX += incX
+        posY += incY
+        if (posY == pattern.size) break
+        if (posX >= pattern[posY].size) {
+            posX %= pattern[posY].size
+        }
+        if (pattern[posY][posX] == '#') treeshit++
+    }
+    println(treeshit)
 }
--- src/main/kotlin/ro/codespace/aoc/day3/Day3b.kt
+++ src/main/kotlin/ro/codespace/aoc/day3/Day3b.kt
@@ -1,5 +1,29 @@
 package ro.codespace.aoc.day3
 
+import ro.codespace.aoc.day
+
 fun main() {
-    TODO()
+    val pattern = day(3).readLines().map { it.toList() }
+
+    listOf(1 to 1, 3 to 1, 5 to 1, 7 to 1, 1 to 2).foldRight(1L) { pair, acc ->
+        acc * checkSlope(pattern, pair.first, pair.second)
+    }.let {
+        println(it)
+    }
+}
+
+private fun checkSlope(pattern: List<List<Char>>, incX: Int, incY: Int): Long {
+    var posY = 0
+    var posX = 0
+    var treeshit = 0L
+    while (posY <= pattern.size) {
+        posX += incX
+        posY += incY
+        if (posY >= pattern.size) break
+        if (posX >= pattern[posY].size) {
+            posX %= pattern[posY].size
+        }
+        if (pattern[posY][posX] == '#') treeshit++
+    }
+    return treeshit
 }
