4586245fb92cb66f92e00e9e0e194de05ec7f9bc
Andrei Gavrila <Andrei.Gavrila@cloudflight.io>
2020-12-06 06:59:47 +0200

    day 5

--- src/main/kotlin/ro/codespace/aoc/day5/Day5a.kt
+++ src/main/kotlin/ro/codespace/aoc/day5/Day5a.kt
@@ -1,5 +1,19 @@
 package ro.codespace.aoc.day5
 
+import ro.codespace.aoc.day
+import kotlin.math.pow
+
+fun String.parseBinary(oneBits: String): Int {
+    var bitValue = 2.0.pow(this.length).toInt()
+    return toList().fold(0) { value, bit ->
+        bitValue /= 2
+        if (bit in oneBits) value + bitValue else value
+    }
+}
+
 fun main() {
-    TODO()
+    day(5).readLines().maxByOrNull {
+        it.parseBinary("BR")
+    }?.let { println(it.parseBinary("BR")) }
+
 }
--- src/main/kotlin/ro/codespace/aoc/day5/Day5b.kt
+++ src/main/kotlin/ro/codespace/aoc/day5/Day5b.kt
@@ -1,5 +1,15 @@
 package ro.codespace.aoc.day5
 
+import ro.codespace.aoc.day
+
+fun getMissing(values: List<Int>): Long {
+    return (values.first().toLong() + values.last().toLong()) * (values.size+1) / 2 - values.sum()
+}
+
 fun main() {
-    TODO()
+        day(5).readLines().map {
+            it.parseBinary("BR")
+        }.sorted().let {
+            println(getMissing(it))
+        }
 }
