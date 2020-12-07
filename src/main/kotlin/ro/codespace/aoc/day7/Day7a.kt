fe042331cbdf5ff077f2ab5bc470d06e86c91f74
Andrei Gavrila <Andrei.Gavrila@cloudflight.io>
2020-12-07 07:24:40 +0200

    day 7

--- src/main/kotlin/ro/codespace/aoc/day7/Day7a.kt
+++ src/main/kotlin/ro/codespace/aoc/day7/Day7a.kt
@@ -1,5 +1,41 @@
 package ro.codespace.aoc.day7
 
+import ro.codespace.aoc.day
+import java.util.*
+
 fun main() {
-    TODO()
+    listOf(day("7test"), day(7)).forEach {
+        val graph = mutableMapOf<String, MutableList<String>>()
+        it.readLines().map {
+            val (outerColor, interior) = it.split("bags contain")
+            val innerColors = interior.split(",").map {
+                val words = it.split(" ")
+                if (words[1] == "no") return@map 0 to ""
+                val count = words[1].toInt()
+                val color = words.subList(2, words.size - 1).joinToString(" ")
+                count to color
+            }
+            outerColor.trim() to innerColors
+        }.forEach { node ->
+            node.second.filter { it.first != 0 }.forEach {
+                if (it.second !in graph) {
+                    graph[it.second] = mutableListOf()
+                }
+                graph[it.second]!!.add(node.first)
+            }
+        }
+
+        val visited = mutableSetOf<String>()
+        val stack = Stack<String>()
+        stack.add("shiny gold")
+
+        while (stack.isNotEmpty()) {
+            val current = stack.pop()
+            for (next in graph[current] ?: listOf()) {
+                stack.add(next)
+                visited.add(next)
+            }
+        }
+        println(visited.size)
+    }
 }
--- src/main/kotlin/ro/codespace/aoc/day7/Day7b.kt
+++ src/main/kotlin/ro/codespace/aoc/day7/Day7b.kt
@@ -1,5 +1,41 @@
 package ro.codespace.aoc.day7
 
+import ro.codespace.aoc.day
+import java.util.*
+
 fun main() {
-    TODO()
+    listOf(day("7test"), day(7)).forEach {
+        val graph = mutableMapOf<String, MutableList<Pair<String, Int>>>()
+        it.readLines().map {
+            val (outerColor, interior) = it.split("bags contain")
+            val innerColors = interior.split(",").map {
+                val words = it.split(" ")
+                if (words[1] == "no") return@map "" to 0
+                val count = words[1].toInt()
+                val color = words.subList(2, words.size - 1).joinToString(" ")
+                color to count
+            }
+            outerColor.trim() to innerColors
+        }.forEach { node ->
+            node.second.filter { it.second != 0 }.forEach {
+                if (node.first !in graph) {
+                    graph[node.first] = mutableListOf()
+                }
+                graph[node.first]!!.add(it)
+            }
+        }
+
+        val stack = Stack<Pair<String, Int>>()
+        stack.add("shiny gold" to 1)
+        var count = -1
+
+        while (stack.isNotEmpty()) {
+            val (current, currentNumber) = stack.pop()
+            count += currentNumber
+            for (next in graph[current] ?: listOf()) {
+                stack.add(next.first to currentNumber * next.second)
+            }
+        }
+        println(count)
+    }
 }
