06e05f364f5cb8829d5e5cc41f776385ae630c4c
Andrei Gavrila <Andrei.Gavrila@cloudflight.io>
2020-12-08 08:41:10 +0200

    couldn't wake up again

--- src/main/kotlin/ro/codespace/aoc/day8/Day8a.kt
+++ src/main/kotlin/ro/codespace/aoc/day8/Day8a.kt
@@ -1,5 +1,27 @@
 package ro.codespace.aoc.day8
 
+import ro.codespace.aoc.day
+
 fun main() {
-    TODO()
+    listOf(day("8t"), day(8)).forEach {
+        var acc = 0
+        var currentLine = 0
+        val linesRan = mutableSetOf<Int>()
+
+        val lines = it.readLines()
+        while (currentLine !in linesRan) {
+            linesRan.add(currentLine)
+            val (op, arg) = lines[currentLine].split(" ")
+
+            when (op) {
+                "nop" -> currentLine++
+                "jmp" -> currentLine += arg.toInt()
+                "acc" -> {
+                    acc += arg.toInt()
+                    currentLine++
+                }
+            }
+        }
+        println(acc)
+    }
 }
--- src/main/kotlin/ro/codespace/aoc/day8/Day8b.kt
+++ src/main/kotlin/ro/codespace/aoc/day8/Day8b.kt
@@ -1,5 +1,45 @@
 package ro.codespace.aoc.day8
 
+import ro.codespace.aoc.day
+
 fun main() {
-    TODO()
+    listOf(day("8t"), day(8)).forEach {
+        val lines = it.readLines()
+        for (line in lines.indices) {
+            if (lines[line].startsWith("acc")) continue
+            val mutatedLines = lines.toMutableList()
+            if (mutatedLines[line].startsWith("nop")) {
+                mutatedLines[line] = mutatedLines[line].replace("nop", "jmp")
+            } else {
+                mutatedLines[line] = mutatedLines[line].replace("jmp", "nop")
+            }
+            val (acc, result) = runCode(mutatedLines)
+            if (result) {
+                println(acc)
+                break
+            }
+        }
+    }
+}
+
+private fun runCode(lines: List<String>): Pair<Int, Boolean> {
+    val linesRan = mutableSetOf<Int>()
+    var acc = 0
+    var currentLine = 0
+
+    while (currentLine !in linesRan) {
+        if (currentLine > lines.lastIndex) return acc to true
+        linesRan.add(currentLine)
+        val (op, arg) = lines[currentLine].split(" ")
+
+        when (op) {
+            "nop" -> currentLine++
+            "jmp" -> currentLine += arg.toInt()
+            "acc" -> {
+                acc += arg.toInt()
+                currentLine++
+            }
+        }
+    }
+    return acc to false
 }
