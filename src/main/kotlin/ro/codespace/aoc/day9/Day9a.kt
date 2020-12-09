ebbddecd09b64f48a60ce54cd1135ab1623b9917
Andrei Gavrila <Andrei.Gavrila@cloudflight.io>
2020-12-09 07:47:20 +0200

    day 9 done

--- src/main/kotlin/ro/codespace/aoc/day9/Day9a.kt
+++ src/main/kotlin/ro/codespace/aoc/day9/Day9a.kt
@@ -1,5 +1,25 @@
 package ro.codespace.aoc.day9
 
+import ro.codespace.aoc.day
+import ro.codespace.aoc.log
+
+fun check(preamble: Set<Long>, target: Long): Boolean {
+    return preamble.any {
+        target - it in preamble
+    }
+}
+
 fun main() {
-    TODO()
+    val input = day(9).readLines().map { it.trim().toLong() }
+
+    val preambleLength = 25
+
+
+    solve(input, preambleLength).log()
+}
+
+fun solve(input: List<Long>, preambleLength: Int): Long {
+    return input.windowed(preambleLength + 1).find {
+        !check(it.subList(0, preambleLength).toSet(), it.last())
+    }!!.last()
 }
--- src/main/kotlin/ro/codespace/aoc/day9/Day9b.kt
+++ src/main/kotlin/ro/codespace/aoc/day9/Day9b.kt
@@ -1,5 +1,32 @@
 package ro.codespace.aoc.day9
 
+import ro.codespace.aoc.day
+import ro.codespace.aoc.log
+
 fun main() {
-    TODO()
+    val input = day(9).readLines().map { it.trim().toLong() }
+
+    val preambleLength = 25
+    val target = solve(input, preambleLength)
+
+
+    var i = 0
+    var j = 2
+
+    while (true) {
+        val sum = input.subList(i, j).sum()
+        if (sum < target) j++
+        else if (sum > target) i++
+        else break
+    }
+
+    input.subList(i, j).log()
+    input.subList(i, j).let {
+        val minOrNull = it.minOrNull()!!
+        minOrNull.log()
+        val maxOrNull = it.maxOrNull()!!
+        maxOrNull.log()
+        println(minOrNull + maxOrNull)
+    }
+    input.subList(i, j).sum().log()
 }
