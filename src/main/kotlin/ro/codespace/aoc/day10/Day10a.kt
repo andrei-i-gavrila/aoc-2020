5ddfdd9aef44108cf957915d5a05337e9cfeb334
Andrei Gavrila <Andrei.Gavrila@cloudflight.io>
2020-12-10 08:10:46 +0200

    day 10

--- src/main/kotlin/ro/codespace/aoc/day10/Day10a.kt
+++ src/main/kotlin/ro/codespace/aoc/day10/Day10a.kt
@@ -1,5 +1,19 @@
 package ro.codespace.aoc.day10
 
+import ro.codespace.aoc.day
+
 fun main() {
-    TODO()
-}
+    listOf(day(10), day("10t")).forEach {
+        val jolts = it.readLines().map { it.toInt() }.sorted()
+        val allJolts = listOf(0) + jolts + listOf(jolts.last() + 3)
+        var oneCount = 0
+        var threeCount = 0
+        allJolts.windowed(2).forEach {
+            when (it[1] - it[0]) {
+                1 -> oneCount++
+                3 -> threeCount++
+            }
+        }
+        println(oneCount * threeCount)
+    }
+ }
--- src/main/kotlin/ro/codespace/aoc/day10/Day10b.kt
+++ src/main/kotlin/ro/codespace/aoc/day10/Day10b.kt
@@ -1,5 +1,28 @@
 package ro.codespace.aoc.day10
 
+import ro.codespace.aoc.day
+import ro.codespace.aoc.log
+
+
+fun ways(jolts: List<Int>): Long {
+    val memo = mutableMapOf<Int, Long>(0 to 1)
+
+
+    jolts.withIndex().drop(1).forEach {
+        val possibleIndices = mutableListOf<Int>()
+        if (it.index >= 1 && it.value - jolts[it.index - 1] <= 3) possibleIndices.add(it.index - 1)
+        if (it.index >= 2 && it.value - jolts[it.index - 2] <= 3) possibleIndices.add(it.index - 2)
+        if (it.index >= 3 && it.value - jolts[it.index - 3] <= 3) possibleIndices.add(it.index - 3)
+        memo[it.index] = possibleIndices.map { memo[it]!! }.sum()
+    }
+    return memo[jolts.lastIndex]!!
+}
+
 fun main() {
-    TODO()
+    listOf(day("10t"), day(10)).forEach {
+        val jolts = it.readLines().map { it.toInt() }.sorted()
+        val allJolts = listOf(0) + jolts + listOf(jolts.last() + 3)
+
+        ways(allJolts).log()
+    }
 }
