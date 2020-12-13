fee0ffe2fa437c896aab3fc3749461efd63efae8
Andrei Gavrila <Andrei.Gavrila@cloudflight.io>
2020-12-13 07:42:04 +0200

    day 13 This was hard. Gotta admit I cheated for part 2. Saw on reddit that part 2 is chinese remainder theorem and copied it from RosettaCode

--- src/main/kotlin/ro/codespace/aoc/day13/Day13a.kt
+++ src/main/kotlin/ro/codespace/aoc/day13/Day13a.kt
@@ -1,5 +1,18 @@
 package ro.codespace.aoc.day13
 
+import ro.codespace.aoc.day
+import ro.codespace.aoc.log
+
 fun main() {
-    TODO()
+    listOf(day("13t"), day(13)).forEach {
+        val (depTimeString, busesString) = it.readLines()
+        val depTime = depTimeString.toInt()
+        val buses = busesString.split(",").filter { it != "x" }.map { it.toInt() }
+
+        buses.map { id: Int ->
+            id to ((depTime / id) * id).let { if (it == depTime) it else it + id }
+        }.minByOrNull { it.second }?.let {
+            (it.second - depTime) * it.first
+        }?.log()
+    }
 }
--- src/main/kotlin/ro/codespace/aoc/day13/Day13b.kt
+++ src/main/kotlin/ro/codespace/aoc/day13/Day13b.kt
@@ -1,5 +1,43 @@
 package ro.codespace.aoc.day13
 
+import ro.codespace.aoc.day
+import ro.codespace.aoc.log
+
+fun multInv(a: Long, b: Long): Long {
+    if (b == 1L) return 1L
+    var aa = a
+    var bb = b
+    var x0 = 0L
+    var x1 = 1L
+    while (aa > 1L) {
+        val q = aa / bb
+        var t = bb
+        bb = aa % bb
+        aa = t
+        t = x0
+        x0 = x1 - q * x0
+        x1 = t
+    }
+    if (x1 < 0L) x1 += b
+    return x1
+}
+
+fun chineseRemainder(n: List<Long>, a: List<Long>): Long {
+    val prod = n.fold(1L) { acc, i -> acc * i }
+    var sum = 0L
+    for (i in n.indices) {
+        val p = prod / n[i]
+        sum += a[i] * multInv(p, n[i]) * p
+    }
+    return sum % prod
+}
 fun main() {
-    TODO()
+    listOf(day("13t"), day(13)).forEach {
+        val (depTimeString, busesString) = it.readLines()
+        val buses = busesString.split(",").withIndex().filter { it.value != "x" }.map { it.value.toLong() to it.index.toLong() }
+
+        buses.log()
+
+        chineseRemainder(buses.map { it.first }, buses.map { it.first - it.second }).log()
+    }
 }
