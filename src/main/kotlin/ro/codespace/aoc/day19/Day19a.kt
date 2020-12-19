bdca4158127e5a2caae712191da3556f7ac9be29
Andrei Gavrila <Andrei.Gavrila@cloudflight.io>
2020-12-19 08:27:01 +0200

    day 19. Regex magic

--- src/main/kotlin/ro/codespace/aoc/day19/Day19a.kt
+++ src/main/kotlin/ro/codespace/aoc/day19/Day19a.kt
@@ -1,5 +1,59 @@
 package ro.codespace.aoc.day19
 
+import ro.codespace.aoc.day
+import ro.codespace.aoc.log
+import ro.codespace.aoc.readGroups
+
+interface Rule {
+    val regexString: String
+}
+
+class BaseRule(val char: Char) : Rule {
+    override val regexString by lazy {
+        char.toString()
+    }
+}
+
+
+class ComposedRule(val subRules: List<Int>, val allRules: Map<Int, Rule>) : Rule {
+    override val regexString by lazy {
+        subRules.map { allRules[it]!! }.joinToString("") { it.regexString }
+    }
+}
+
+class DisjunctionRule(val subRules: List<Rule>) : Rule {
+    override val regexString by lazy {
+        subRules.joinToString("|", prefix = "(", postfix = ")") { it.regexString }
+    }
+}
+
+
+
+
 fun main() {
-    TODO()
+    day(19) {
+        val (rules, inputs) = it.readLines().readGroups()
+
+
+        val allRules = mutableMapOf<Int, Rule>()
+        rules.forEach {
+            val ruleString = it.substringAfter(": ")
+            val index = it.substringBefore(":").toInt()
+
+            allRules[index] = when {
+                ruleString.startsWith("\"") -> BaseRule(ruleString[1])
+                ruleString.contains("|") -> {
+                    DisjunctionRule(ruleString.split(" | ").map { ComposedRule(it.split(" ").map { it.toInt() }, allRules) })
+                }
+                else -> {
+                    ComposedRule(ruleString.split(" ").map { it.toInt() }, allRules)
+                }
+            }
+        }
+
+        val regex = Regex(allRules[0]!!.regexString)
+        inputs.filter {
+            regex.matches(it)
+        }.size.log()
+    }
 }
--- src/main/kotlin/ro/codespace/aoc/day19/Day19b.kt
+++ src/main/kotlin/ro/codespace/aoc/day19/Day19b.kt
@@ -1,5 +1,52 @@
 package ro.codespace.aoc.day19
 
+import ro.codespace.aoc.day
+import ro.codespace.aoc.log
+import ro.codespace.aoc.readGroups
+
+
+class SpecialRule8(val allRules: Map<Int, Rule>) : Rule {
+    override val regexString by lazy {
+        "(" + allRules[42]!!.regexString + ")+"
+    }
+}
+
+class SpecialRule11(val allRules: Map<Int, Rule>) : Rule {
+    override val regexString by lazy {
+        (1..4).map {// manually searched the amount of times required to repeat by running until value did not change
+            "(" + allRules[42]!!.regexString + "{${it}}" + allRules[31]!!.regexString + "{${it}})"
+        }.joinToString("|", prefix = "(", postfix = ")")
+    }
+}
+
 fun main() {
-    TODO()
+    day(19, "t2") {
+        val (rules, inputs) = it.readLines().readGroups()
+
+
+        val allRules = mutableMapOf<Int, Rule>()
+        rules.forEach {
+            val ruleString = it.substringAfter(": ")
+            val index = it.substringBefore(":").toInt()
+
+
+            allRules[index] = when {
+                index == 8 -> SpecialRule8(allRules)
+                index == 11 -> SpecialRule11(allRules)
+                ruleString.startsWith("\"") -> BaseRule(ruleString[1])
+                ruleString.contains("|") -> {
+                    DisjunctionRule(ruleString.split(" | ").map { ComposedRule(it.split(" ").map { it.toInt() }, allRules) })
+                }
+                else -> {
+                    ComposedRule(ruleString.split(" ").map { it.toInt() }, allRules)
+                }
+            }
+        }
+
+        val pattern = allRules[0]!!.regexString
+        val regex = Regex(pattern)
+        inputs.filter {
+            regex.matches(it)
+        }.size.log()
+    }
 }
