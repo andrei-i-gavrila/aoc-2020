80e07f3cc162ba06fcf7813ad2284ab647b3b2e2
Andrei Gavrila <Andrei.Gavrila@cloudflight.io>
2020-12-22 08:50:37 +0200

    day 22

--- src/main/kotlin/ro/codespace/aoc/day22/Day22a.kt
+++ src/main/kotlin/ro/codespace/aoc/day22/Day22a.kt
@@ -1,5 +1,43 @@
 package ro.codespace.aoc.day22
 
+import ro.codespace.aoc.day
+import ro.codespace.aoc.log
+import ro.codespace.aoc.readGroups
+
+
+fun calcScore(deck: ArrayDeque<Int>): Int {
+    return deck.mapIndexed { index, i -> i * (deck.size - index) }.also { it.log() }.sum()
+}
+
 fun main() {
-    TODO()
+    day(22) {
+        val (playerA, playerB) = it.readLines().readGroups().map {
+            it.drop(1).map { it.toInt() }.let {
+                ArrayDeque(it)
+            }
+        }
+        while (true) {
+            val topA = playerA.removeFirst()
+            val topB = playerB.removeFirst()
+
+            if (topA > topB) {
+                playerA.addLast(topA)
+                playerA.addLast(topB)
+            } else {
+                playerB.addLast(topB)
+                playerB.addLast(topA)
+            }
+
+            if (playerA.isEmpty()) {
+                playerB.log()
+                calcScore(playerB).log()
+                break
+            }
+            if (playerB.isEmpty()) {
+                playerA.log()
+                calcScore(playerA).log()
+                break
+            }
+        }
+    }
 }
--- src/main/kotlin/ro/codespace/aoc/day22/Day22b.kt
+++ src/main/kotlin/ro/codespace/aoc/day22/Day22b.kt
@@ -1,5 +1,68 @@
 package ro.codespace.aoc.day22
 
+import ro.codespace.aoc.day
+import ro.codespace.aoc.log
+import ro.codespace.aoc.readGroups
+
+fun playGame(deckA: ArrayDeque<Int>, deckB: ArrayDeque<Int>): Boolean {
+
+    val configurations = hashSetOf<String>()
+    while (true) {
+        val configuration = deckA.joinToString(",") + "#" + deckB.joinToString(",")
+        if (configuration in configurations) {
+            return true
+        }
+        configurations.add(configuration)
+
+        val topA = deckA.removeFirst()
+        val topB = deckB.removeFirst()
+
+        val aWins = if (topA <= deckA.size && topB <= deckB.size) {
+//            println("subgame")
+//            deckA.log()
+//            deckB.log()
+            playGame(ArrayDeque(deckA.subList(0, topA)), ArrayDeque(deckB.subList(0, topB)))
+        } else {
+            topA > topB
+        }
+
+        if (aWins) {
+            deckA.addLast(topA)
+            deckA.addLast(topB)
+        } else {
+            deckB.addLast(topB)
+            deckB.addLast(topA)
+        }
+
+
+
+        if (deckA.isEmpty()) {
+            return false
+        }
+        if (deckB.isEmpty()) {
+            return true
+        }
+    }
+}
+
+
 fun main() {
-    TODO()
+    day(22, "t2") {
+        val (playerA, playerB) = it.readLines().readGroups().map {
+            it.drop(1).map { it.toInt() }.let {
+                ArrayDeque(it)
+            }
+        }
+
+        playGame(playerA, playerB).let {
+            if (it) {
+                playerA.log()
+                calcScore(playerA).log()
+            } else {
+                playerB.log()
+                calcScore(playerB).log()
+            }
+        }
+
+    }
 }
